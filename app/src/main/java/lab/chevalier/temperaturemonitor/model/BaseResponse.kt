d8ed417b2c5431f7fe985b305d1f4368f03884a6
aderesie <aderesie.m.s@gmail.com>
2020-12-20 23:09:07 +0700

    Update response

--- app/src/main/java/lab/chevalier/temperaturemonitor/model/BaseResponse.kt
+++ app/src/main/java/lab/chevalier/temperaturemonitor/model/BaseResponse.kt
@@ -6,8 +6,6 @@ import com.squareup.moshi.JsonClass
 
 @JsonClass(generateAdapter = true)
 data class BaseResponse(
-    @Json(name = "channel")
-    val channel: Channel,
     @Json(name = "feeds")
     val feeds: List<Feed>
 )
