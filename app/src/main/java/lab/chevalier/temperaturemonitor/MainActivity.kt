bf0fcd39d25496ca10a6485a7a5b6427d5f786c0
aderesie <aderesie.m.s@gmail.com>
2020-12-20 23:55:07 +0700

    Menampilkan data

--- app/src/main/java/lab/chevalier/temperaturemonitor/MainActivity.kt
+++ app/src/main/java/lab/chevalier/temperaturemonitor/MainActivity.kt
@@ -1,11 +1,55 @@
 package lab.chevalier.temperaturemonitor
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import lab.chevalier.temperaturemonitor.adapter.PBSAdapter
+import lab.chevalier.temperaturemonitor.api.ApiStatus
+import lab.chevalier.temperaturemonitor.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private val adapter = PBSAdapter()
+
+    private val viewModel: MainViewModel by lazy {
+        ViewModelProvider(this).get(MainViewModel::class.java)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        this.binding = DataBindingUtil.setContentView(this, (R.layout.activity_main))
+        this.binding.rvData.apply {
+            this.adapter = this@MainActivity.adapter
+            this.layoutManager = LinearLayoutManager(this@MainActivity)
+        }
+        this.initViewModel()
+    }
+
+    private fun initViewModel() {
+        this.viewModel.apply {
+            this.getFeeds().observe(this@MainActivity, { this@MainActivity.adapter.setData(it) })
+            this.getApiStatus().observe(this@MainActivity, { this@MainActivity.updateProgress(it) })
+        }
+    }
+
+    private fun updateProgress(status: ApiStatus) {
+        this.binding.apply {
+            when (status) {
+                ApiStatus.LOADING -> {
+                    progressBar.visibility = View.VISIBLE
+                }
+                ApiStatus.SUCCESS -> {
+                    progressBar.visibility = View.GONE
+                    rvData.visibility = View.VISIBLE
+                }
+                ApiStatus.FAILED -> {
+                    progressBar.visibility = View.GONE
+                }
+            }
+        }
     }
 }
